699705ea85bb1c2d98401d4975f44702d871697f
genzirodoto <148872280+genzirodoto@users.noreply.github.com>
2023-12-13 16:10:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Вшивцева Тимофея Андреевича";
 
-  console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
